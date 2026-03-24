bac6b79bd64956be3d0d72e9b33eaf0c53b3816f
Chester Aldrin Uy <chesteruytech@gmail.com>
2026-03-24 22:06:21 +0800

    Can update passenger details

--- src/main/java/com/dlsu/ccinfom/seams/passengers.java
+++ src/main/java/com/dlsu/ccinfom/seams/passengers.java
@@ -7,14 +7,14 @@ public class passengers {
     public int pID, pPassport, pNumber;
     public String pLN, pFN, pUsername, pEmail, pPassword;
 
-    public ArrayList<Integer> pID_list = new ArrayList<>();
-    public ArrayList<Integer> pPassport_list = new ArrayList<>();
-    public ArrayList<String> pLN_list = new ArrayList<>();
-    public ArrayList<String> pFN_list = new ArrayList<>();
-    public ArrayList<Integer> pNumber_list = new ArrayList<>();
-    public ArrayList<String> pUsername_list = new ArrayList<>();
-    public ArrayList<String> pEmail_list = new ArrayList<>();
-    public ArrayList<String> pPassword_list = new ArrayList<>();
+//    public ArrayList<Integer> pID_list = new ArrayList<>();
+//    public ArrayList<Integer> pPassport_list = new ArrayList<>();
+//    public ArrayList<String> pLN_list = new ArrayList<>();
+//    public ArrayList<String> pFN_list = new ArrayList<>();
+//    public ArrayList<Integer> pNumber_list = new ArrayList<>();
+//    public ArrayList<String> pUsername_list = new ArrayList<>();
+//    public ArrayList<String> pEmail_list = new ArrayList<>();
+//    public ArrayList<String> pPassword_list = new ArrayList<>();
 
     public passengers() {}
 
@@ -22,11 +22,11 @@ public class passengers {
         try {
             // 1. Connect to SeAMS Database
             Connection seamsDB;
-            seamsDB = DriverManager.getConnection("jdbc:mysql://localhost:3306/seams_db", "root", "root");
+            seamsDB = DriverManager.getConnection("jdbc:mysql://localhost:3306/seamsDB", "root", "root");
             if(seamsDB.isValid(1)) {
                 System.out.println("Connection successful");
 
-                // 2. Prepare an SQL Statement
+                // 2. Prepare an SQL Passenger Data
                 PreparedStatement ps = seamsDB.prepareStatement("");
                 ResultSet rs = ps.executeQuery();
                 while(rs.next()) {
@@ -57,9 +57,45 @@ public class passengers {
         return false;
     }
 
+    public boolean updatePassenger(){
+        try {
+            // 1. Connect to SeAMS Database
+            Connection seamsDB;
+            seamsDB = DriverManager.getConnection("jdbc:mysql://localhost:3306/seamsDB", "root", "root");
+            if(seamsDB.isValid(1)) {
+                System.out.println("Connection successful");
+
+                // 2. Prepare an SQL Statement
+                PreparedStatement ps = seamsDB.prepareStatement("");
+                ResultSet rs = ps.executeQuery();
+                while(rs.next()) {
+                    pID = rs.getInt("pID");
+                }
+
+                // 3. Save the new Passenger
+                ps = seamsDB.prepareStatement("");
+                ps.setInt(1, pID);
+                ps.executeUpdate();
+
+                ps.close();
+                seamsDB.close();
+
+                System.out.println("Passenger Updated successfully");
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    // Sample dummy data
     static void main() {
         passengers p = new passengers();
         p.pFN = "Chester Aldrin";
         p.registerPassenger();
+        p.pID = 1;
+        p.pFN = "Hanna";
+        p.updatePassenger();
     }
 }
