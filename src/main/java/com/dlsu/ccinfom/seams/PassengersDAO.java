9d543c49f578c47c9be965b788122204ab800f50
Chester Aldrin Uy <chesteruytech@gmail.com>
2026-03-26 05:47:16 +0800

    PassengersDAO functionality adjustments. The rest are minor changes.

--- src/main/java/com/dlsu/ccinfom/seams/PassengersDAO.java
+++ src/main/java/com/dlsu/ccinfom/seams/PassengersDAO.java
@@ -11,15 +11,15 @@ public class PassengersDAO {
             if(seamsDB.isValid(1)) {
                 System.out.println("Connection successful");
 
-                // 2. Prepare an SQL Passenger Data
-                PreparedStatement ps = seamsDB.prepareStatement("");
+                // 2. Prepare an SQL Passenger Data to get the next passenger
+                PreparedStatement ps = seamsDB.prepareStatement("SELECT MAX(passengerID) + 1 FROM passengers");
                 ResultSet rs = ps.executeQuery();
                 while(rs.next()) {
                     passengers.pID = rs.getInt("pID");
                 }
 
                 // 3. Save the new Passenger
-                ps = seamsDB.prepareStatement("");
+                ps = seamsDB.prepareStatement("INSERT INTO passengers (passengerID, passportNumber, lastName, firstName, contactNumber, username, contactEmail, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
                 ps.setInt(1, passengers.pID);
                 ps.setInt(2, passengers.pPassport);
                 ps.setString(3, passengers.pLN);
@@ -50,16 +50,17 @@ public class PassengersDAO {
             if(seamsDB.isValid(1)) {
                 System.out.println("Connection successful");
 
-                // 2. Prepare an SQL Statement
-                PreparedStatement ps = seamsDB.prepareStatement("");
-                ResultSet rs = ps.executeQuery();
-                while(rs.next()) {
-                    passengers.pID = rs.getInt("pID");
-                }
-
-                // 3. Save the new Passenger
-                ps = seamsDB.prepareStatement("");
+                // 2. Prepare an SQL Statement to update the passenger data
+                PreparedStatement ps = seamsDB.prepareStatement("UPDATE aircraft SET passengerID=?, passportNumber=?, lastName=?, firstName=?, contactNumber=?, username=?, contactEmail=?, password=? WHERE passengerID=?");
                 ps.setInt(1, passengers.pID);
+                ps.setInt(2, passengers.pPassport);
+                ps.setString(3, passengers.pLN);
+                ps.setString(4, passengers.pFN);
+                ps.setInt(5, passengers.pNumber);
+                ps.setString(6, passengers.pUsername);
+                ps.setString(7, passengers.pEmail);
+                ps.setString(8, passengers.pPassword);
+                ps.executeUpdate();
                 ps.executeUpdate();
 
                 ps.close();
